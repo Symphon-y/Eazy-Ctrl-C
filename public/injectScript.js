545832769a0857d13d59e5911f0c14cce044835b
Symphon-y <travisredden@outlook.com>
2022-09-03 09:56:11 -0500

    progress on html injection

---
+++ public/injectScript.js
@@ -0,0 +1,6 @@
+const popup_entry_point = document.createElement('div');
+let reactJS_script = document.createElement('script');
+popup_entry_point.id = 'popupButton';
+
+popup_entry_point.appendChild(reactJS_script);
+document.querySelector('body').appendChild(popup_entry_point);
---
+++ src/components/Form.jsx
@@ -0,0 +1,11 @@
+import React, { useContext } from 'react';
+
+const Form = () => {
+  return (
+    <div>
+      <h1>This is a form</h1>
+    </div>
+  );
+};
+
+export default Form;
