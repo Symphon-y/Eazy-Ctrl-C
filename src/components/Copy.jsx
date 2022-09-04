cd54598d351db6ad3f7092fdc15f929de64a9c9e
Symphon-y <travisredden@outlook.com>
2022-09-04 02:05:23 -0500

    added display for local storage keys on page, added delete function, added state

--- src/components/Copy.jsx
+++ src/components/Copy.jsx
@@ -1,12 +1,31 @@
-import React, { useContext } from 'react';
+import React, { useState } from 'react';
+import { Button, Box } from '@mui/material';
 
 const Copy = () => {
-  console.log(Object.keys(localStorage));
-  return (
-    <div>
-      <h1>This is a Copy Page</h1>
-    </div>
-  );
+  const [keys, setKeys] = useState(Object.keys(localStorage).sort());
+  const removeFromStorage = (event, label) => {
+    localStorage.removeItem(label);
+    setKeys(Object.keys(localStorage).sort());
+    event.preventDefault();
+  };
+
+  const storageItems = () => {
+    return keys.map((item) => {
+      return (
+        <Box component='span' sx={{ p: 2 }}>
+          <Button variant='contained'>{item}</Button>
+          <Button
+            variant='contained'
+            onClick={(e) => {
+              removeFromStorage(e, item);
+            }}>
+            Delete
+          </Button>
+        </Box>
+      );
+    });
+  };
+  return <div>{storageItems()}</div>;
 };
 
 export default Copy;
