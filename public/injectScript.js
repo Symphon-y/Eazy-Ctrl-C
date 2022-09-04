38f31e45e958e1bac22b13b39d39d860fa1fd507
Symphon-y <travisredden@outlook.com>
2022-09-04 01:13:44 -0500

    removed unused scripts

--- public/injectScript.js
+++
@@ -1,6 +0,0 @@
-const popup_entry_point = document.createElement('div');
-let reactJS_script = document.createElement('script');
-popup_entry_point.id = 'popupButton';
-
-popup_entry_point.appendChild(reactJS_script);
-document.querySelector('body').appendChild(popup_entry_point);
